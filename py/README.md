bc1f109e8b2889d3f54c509df2e01f94c7af8daf
Karen Sittig <kasittig@gmail.com>
2024-12-07 17:01:21 -0500

    Add pre-commit hooks

---
+++ py/README.md
@@ -0,0 +1,1 @@
+[![Checked with pyright](https://microsoft.github.io/pyright/img/pyright_badge.svg)](https://microsoft.github.io/pyright/)
--- py/hello.py
+++ py/hello.py
@@ -1,5 +1,14 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = ["click>=8.1.7"]
+# ///
+
+import click
+
+
+@click.command()
 def main():
-    print("Hello from py!")
+    print("Hello from Advent of Code 2024!")
 
 
 if __name__ == "__main__":
